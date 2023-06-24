3c7a509643ff2181e4e8bfb102d74e8ed83da5cb
masha <mativaclub@gmail.com>
2023-06-24 10:23:37 +0300

    Initial commit

---
+++ src/main/java/pro/sky/finalExam/questions_for_course2/QuestionsForCourse2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.finalExam.questions_for_course2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsForCourse2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsForCourse2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/finalExam/questions_for_course2/QuestionsForCourse2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.finalExam.questions_for_course2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsForCourse2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
